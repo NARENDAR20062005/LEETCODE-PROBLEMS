c4c602d52d6b9daab389b4a282a5e02504afc6a6
NARENDAR <162038232+NARENDAR20062005@users.noreply.github.com>
2025-04-11 12:11:09 +0530

    Time: 12 ms (49.55%), Space: 59.7 MB (14.71%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer,Integer> m =new HashMap<>();
+        for(int i =0;i<nums.length;i++){
+            if(m.containsKey(nums[i])){
+                return true;
+            }else{
+                m.put(nums[i],i);
+            }
+        }
+        return false;
+    }
+}
