3ceddd6331edef85b674d9578dc1e1730873567f
NARENDAR <162038232+NARENDAR20062005@users.noreply.github.com>
2025-04-21 15:57:16 +0530

    Time: 1 ms (98.96%), Space: 41.5 MB (26.31%) - LeetHub

---
+++ 2561-number-of-distinct-averages/2561-number-of-distinct-averages.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int distinctAverages(int[] nums) {
+        if(nums.length <=1) return 0;
+        Arrays.sort(nums);
+        Set<Double> set = new HashSet<>();
+
+        int left=0,right=nums.length-1;
+
+        while(left<right){
+            double avg = (nums[left]+nums[right])/2.0;
+            set.add(avg);
+            left++;
+            right--;
+        }
+        return set.size();
+        
+    }
+}
